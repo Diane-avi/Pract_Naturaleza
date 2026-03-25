2a46ad940ba96b101038cd117e0ded40ec319028
Diane-avi <dianelinoavila@gmail.com>
2026-03-24 23:39:47 -0600

    Delete js directory

--- js/app.js
+++
@@ -1,7 +0,0 @@
-function mostrarPantalla(id) {
-  document.querySelectorAll('.pantalla').forEach(p => {
-    p.classList.remove('activa');
-  });
-
-  document.getElementById(id).classList.add('activa');
-}
